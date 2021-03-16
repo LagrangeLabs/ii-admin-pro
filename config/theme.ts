67dab355c2b5ee9731e2feb30e02743e097d2bc5
大黄蜂(张明瑞) <zhangmingrui@mail.i-i.ai>
2021-03-16 20:56:38 +0800

    ✨feat: antd主题更新
    根据UI规范，更新antd主题配置

--- config/theme.ts
+++ config/theme.ts
@@ -1,8 +1,8 @@
 export default {
   '@primary-color': '#0085ff', // 全局主色
   '@link-color': '#0085ff', // 链接色
-  '@success-color': '#2FC37C', // 成功色
-  '@warning-color': '#faad14', // 警告色
+  '@success-color': '#2DD17C', // 成功色
+  '@warning-color': '#FF7B24', // 警告色
   '@error-color': '#FF493D', // 错误色
   '@font-size-base': '13px', // 主字号
   '@heading-color': '#334355', // 标题色
@@ -13,4 +13,11 @@ export default {
   '@border-color-base': '#DEE6EE', // 边框色
   '@form-item-margin-bottom': '20px',
   '@input-placeholder-color': 'rgba(51,67,85,0.4)', // input placeholder 颜色
+
+  // tabs
+  '@tabs-horizontal-padding': '9px 36px',
+  '@tabs-card-gutter': '0',
+  '@tabs-card-height': '36px',
+  // tables
+  '@table-header-bg': '#EAEEF2',
 }
--- src/pages/demo/index.tsx
+++ src/pages/demo/index.tsx
@@ -1,7 +1,7 @@
 import React, { Fragment, useEffect } from 'react'
 import { connect, Dispatch, Link } from 'umi'
 import type { ConnectState } from '@/models/connect'
-import { Table } from 'antd'
+import { Table, Tabs } from 'antd'
 import type { PluginModelState } from '@/utils/pluginModel'
 import type { IDemoRow } from './interface'
 import PageHeader from '@/components/page-header'
@@ -9,6 +9,8 @@ import PageHeader from '@/components/page-header'
 interface IProps extends PluginModelState<IDemoRow> {
   getDemoList: () => void
 }
+
+const { TabPane } = Tabs
 
 const Demo = (props: IProps) => {
   const { tableList, getDemoList } = props
@@ -23,11 +25,36 @@ const Demo = (props: IProps) => {
     { title: '性别', dataIndex: 'gender' },
     { title: '操作', render: () => <Link to="">查看详情</Link> },
   ]
+  const size = 'default'
 
   return (
     <Fragment>
       <PageHeader title="demo页面" />
       <Table columns={columns} dataSource={tableList} rowKey={(row: IDemoRow) => row.id} />
+
+      <h3>Tabs组件</h3>
+      <Tabs defaultActiveKey="1" size={size} style={{ marginBottom: 32 }}>
+        <TabPane tab="Tab 1" key="1">
+          Content of tab 1
+        </TabPane>
+        <TabPane tab="Tab 2" key="2">
+          Content of tab 2
+        </TabPane>
+        <TabPane tab="Tab 3" key="3">
+          Content of tab 3
+        </TabPane>
+      </Tabs>
+      <Tabs defaultActiveKey="1" type="card" size={size}>
+        <TabPane tab="Card Tab 1" key="1">
+          Content of card tab 1
+        </TabPane>
+        <TabPane tab="Card Tab 2" key="2">
+          Content of card tab 2
+        </TabPane>
+        <TabPane tab="Card Tab 3" key="3">
+          Content of card tab 3
+        </TabPane>
+      </Tabs>
     </Fragment>
   )
 }
